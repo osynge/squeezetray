1b4d7951f066846e1c4bb3079cac0f0389c5d192
Owen Synge <owen.synge@desy.de>
2012-06-14 23:19:40 +0200

    handleConnectionStrChange. move to more MVC pattern. Seperate MainApp from frame handling duty.

--- sqtray/wxMainApp.py
+++ sqtray/wxMainApp.py
@@ -6,6 +6,7 @@ from sqtray.JrpcServer import squeezeConCtrl
 from wxEvents import EVT_RESULT_CONNECTED_ID
 from wxEvents import EVT_RESULT_PLAYERS_ID
 from wxEvents import EVT_RESULT_CURRENT_TRACK_ID
+from wxEvents import EVT_RESULT_CONNECTION_ID
 from sqtray.wxEvents import ResultEvent2
 
 
@@ -42,26 +43,32 @@ class FrmCtrl:
         self.closeSettings(None)
         self.tb.Destroy()
 
-    def handleConnectionChange(self,value,window):
+
+    def handleConnectionStrChange(self,value):
+        if (self.Example != None):
+            wx.PostEvent(self.Example, ResultEvent2(EVT_RESULT_CONNECTION_ID,value))
+            
+    def handleConnectionChange(self,value, window):
         wx.PostEvent(self.tb, ResultEvent2(EVT_RESULT_CONNECTED_ID,value))
         if (self.Example != None):
             wx.PostEvent(self.Example, ResultEvent2(EVT_RESULT_PLAYERS_ID,value))
     
-    def handlePlayersChange(self,value,window,asda):
+    def handlePlayersChange(self,value,window = None,asda= None):
         wx.PostEvent(self.tb, ResultEvent2(EVT_RESULT_PLAYERS_ID,value))
         if (self.Example != None):
             wx.PostEvent(self.Example, ResultEvent2(EVT_RESULT_PLAYERS_ID,value))
         
     def handleTrackChange(self,value,other):
-        wx.PostEvent(self.tb, ResultEvent2(EVT_RESULT_CURRENT_TRACK_ID,value))    
+        wx.PostEvent(self.tb, ResultEvent2(EVT_RESULT_CURRENT_TRACK_ID,value))
 
 class myapp(wx.App):
     def __init__(self):
         super(myapp, self).__init__()
         
         self.model = squeezeConMdle()
-        self.GuiPlayer = Observable(None)
-        self.GuiPlayerDefault = Observable(None)
+        self.model.GuiPlayer = Observable(None)
+        self.model.GuiPlayerDefault = Observable(None)
+        self.model.CbPlayersAvailableAdd(self.OnPlayerAvailable)
         self.SqueezeServerPort = Observable(9000)
         self.SqueezeServerPort.addCallback(self.OnSqueezeServerPort)
         self.cfg = wx.FileConfig(appName="ApplicationName", 
@@ -69,8 +76,7 @@ class myapp(wx.App):
                                     localFilename=".squeezetray.cfg", 
                                     style=wx.CONFIG_USE_LOCAL_FILE)
         self.squeezeConCtrl = squeezeConCtrl(self.model)
-        
-        
+             
         self.frmCtrl = FrmCtrl(self.model )
         self.frmCtrl.setApp(self)
         self.frmCtrl.setCfg(self.cfg)
@@ -78,21 +84,43 @@ class myapp(wx.App):
         
         #print "tb=%s" %self.tb
         self.tb.cfg = self.cfg
+        self.model.GuiPlayer.addCallback(self.frmCtrl.handleConnectionStrChange)
         self.squeezeConCtrl.CbConnectionAdd(self.frmCtrl.handleConnectionChange,None)
         self.model.CbPlayersAvailableAdd(self.frmCtrl.handlePlayersChange,None)
+        self.model.CbPlayersAvailableAdd(self.OnPlayers)
         self.model.CbChurrentTrackAdd(self.frmCtrl.handleTrackChange,None)
+        self.model.connectionStr.addCallback(self.frmCtrl.handlePlayersChange)
         self.configRead()
-
+    def OnPlayers(self,connected,playerlist):
+        # If Not connected set None
+        if not connected:
+            if None != self.model.GuiPlayer.get():
+                self.model.GuiPlayer.set(None)
+            return
+        # Try to Apply Default Player
+        DefaultPlayer = self.model.GuiPlayerDefault.get()
+        if DefaultPlayer in playerlist:
+            OldDefaultPlayer = self.model.GuiPlayer.get()
+            if OldDefaultPlayer != DefaultPlayer:
+                self.model.GuiPlayer.set(DefaultPlayer)
+            return
+        # Try to find any player
+        for index in range(len(self.model.playerList)):
+            Name = self.model.playerList[index].name.get()
+            if Name != None:
+                self.model.GuiPlayer.set(unicode(Name))
+                return
+            
     def configRead(self):
+        # Set Host
         squeezeServerHost = 'localhost'
         if self.cfg.Exists('squeezeServerHost'):
             squeezeServerHost = self.cfg.Read('squeezeServerHost')
         OldSqueezeServerHost = self.model.host.get()
-        
         if squeezeServerHost != OldSqueezeServerHost:
-           
             self.model.host.set(squeezeServerHost)
         self.SetSqueezeServerHost(squeezeServerHost)
+        # Set Port
         squeezeServerPort = 9000
         if self.cfg.Exists('squeezeServerPort'):
             try:
@@ -106,21 +134,24 @@ class myapp(wx.App):
         if squeezeServerPort != OldSqueezeServerPort:
             self.model.port.set(squeezeServerPort)
         
-        
+        # Set Player
         SqueezeServerPlayer = None
         if self.cfg.Exists('SqueezeServerPlayer'):
             SqueezeServerPlayer = self.cfg.Read('SqueezeServerPlayer')
-            
         self.SetSqueezeServerPlayer(SqueezeServerPlayer)
+        self.model.GuiPlayerDefault.set(SqueezeServerPlayer)
         self.squeezeConCtrl.RecConnectionOnline()
         
     def configSave(self):
-        self.cfg.Write("squeezeServerHost", self.GetSqueezeServerHost())
-        self.cfg.WriteInt("squeezeServerPort", self.GetSqueezeServerPort())
-        self.cfg.Write("SqueezeServerPlayer", self.GetSqueezeServerPlayer())
+        print 'saving'
+        self.cfg.Write("squeezeServerHost", self.model.host.get())
+        self.cfg.WriteInt("squeezeServerPort", self.model.port.get())
+        SqueezeServerPlayer = self.GetSqueezeServerPlayer()
+        self.cfg.Write("SqueezeServerPlayer", self.model.GuiPlayerDefault.get())
         self.cfg.Flush()
         
-
+    def OnPlayerAvailable(self,AvailablePlayers,extra):
+        print "AvailablePlayers",AvailablePlayers,extra
     def SetSqueezeServerHost(self,host):
         OldHost = self.model.host.get()
         if OldHost != host:
