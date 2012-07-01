87b0bd7b69bbb9cb4c6736f7e47d1a5c4a879291
Owen Synge <owen.synge@desy.de>
2012-07-01 15:29:00 +0200

    ObservableDict

--- sqtray/models.py
+++ sqtray/models.py
@@ -1,4 +1,4 @@
-
+import UserDict
 
 class Observable:
     def __init__(self, initialValue=None):
@@ -24,6 +24,34 @@ class Observable:
 
     def unset(self):
         self.data = None
+
+class ObservableDict( UserDict.DictMixin):
+    def __init__(self):
+        self._dict = {}
+    def __getitem__(self, item):
+        self.callbacks = {}
+        if not item in self._dict.keys():
+            raise KeyError("Item %s does not exist" % item)
+        return self._dict[item]
+    def addCallback(self, func):
+        self.callbacks[func] = 1
+    def delCallback(self, func):
+        del self.callback[func]
+    
+    def _docallbacks(self,key):
+        for func in self.callbacks:
+            func(self._dict)
+    def __setitem__(self, item, value):
+        if item in self:
+            del self[item]
+        self._dict[item] = value
+        self._docallbacks(item)
+    def __delitem__(self, item):
+        if not item in self._dict:
+            raise KeyError("File %s does not exist" % fn)
+        self._docallbacks(item)
+    def keys(self):
+        return self._dict.keys()
 
 class squeezePlayerMdl:
     def __init__(self,index,identifier = None, Name = None):
@@ -65,7 +93,7 @@ class squeezeConMdle:
         self.SocketErrMsg = Observable("")
         
         self.playerList = []
-        self.Players = {}
+        self.Players = ObservableDict()
         self.CbPlayersAvailable= []
         self.CbChurrentTrack = []
         self.host.addCallback(self.OnHostChange)
