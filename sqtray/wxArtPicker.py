40d2c3439bc41c5d2f08e862e737094366f0ecaf
Owen Synge <owen.synge@desy.de>
2012-12-16 22:42:33 +0100

    fixed loads of name issue bugs and also made the icons load based around module directory.

--- sqtray/wxArtPicker.py
+++ sqtray/wxArtPicker.py
@@ -2,14 +2,32 @@ import wx
 import logging
 import os
 import wxIcons
-
+def getProgramFolder():
+    moduleFile = __file__
+    moduleDir = os.path.split(os.path.abspath(moduleFile))[0]
+    programFolder = os.path.abspath(moduleDir)
+    return programFolder + "/.."
    
 class MyArtProvider(wx.ArtProvider):   
     def __init__(self):
         self.log = logging.getLogger("MyArtProvider")
+        self.baseFolder = getProgramFolder()
         wx.ArtProvider.__init__(self)   
         self.defaultIconMappingActionName = { 
-            'ART_APPLICATION_STATUS_DISCONECTED' : set(["media_playback_start_16x16.png"]),
+            'ART_APPLICATION_STATUS_DISCONECTED' : set(["application_disconected_16x16.png",
+                "application_disconected_22x22.png",
+                "application_disconected_24x24.png",
+                "application_disconected_32x32.png",
+                "application_disconected_48x48.png",
+                "application_disconected_72x72.png",
+                "application_disconected_128x128.png"]),
+            'ART_APPLICATION_STATUS_CONNECTED' : set(["application_connected_16x16.png",
+                "application_connected_22x22.png",
+                "application_connected_24x24.png",
+                "application_connected_32x32.png",
+                "application_connected_48x48.png",
+                "application_connected_72x72.png",
+                "application_connected_128x128.png"]),
             'ART_PLAYER_PLAY' : set(["media_playback_start_16x16.png",
                 "media_playback_start_22x22.png",
                 "media_playback_start_24x24.png",
@@ -41,27 +59,33 @@ class MyArtProvider(wx.ArtProvider):
             
             }
         self.defaultIconMappingSize = { 
-            (16, 16) : set(["media_playback_start_16x16.png",
+            (16, 16) : set(["application_disconected_16x16.png",
+                "application_connected_16x16.png",
+                "media_playback_start_16x16.png",
                 "media_playback_pause_16x16.png",
-                "media_playback_stop_16x16",
+                "media_playback_stop_16x16.png",
                 "media_record_16x16.png",
                 "media_seek_backward_16x16.png",
                 "media_seek_forward_16x16.png",
                 "media_skip_backward_16x16.png",
                 "media_skip_forward_16x16.png",
                 "media_eject_16x16.png"]),
-            (24, 24) : set(["media_playback_start_24x24.png",
+            (24, 24) : set(["application_disconected_24x24.png",
+                "application_connected_24x24.png",
+                "media_playback_start_24x24.png",
                 "media_playback_pause_24x24.png",
-                "media_playback_stop_24x24",
+                "media_playback_stop_24x24.png",
                 "media_record_24x24.png",
                 "media_seek_backward_24x24.png",
                 "media_seek_forward_24x24.png",
                 "media_skip_backward_24x24.png",
                 "media_skip_forward_24x24.png",
                 "media_eject_24x24.png"]),
-            (32, 32) : set(["media_playback_start_32x32.png",
+            (32, 32) : set(["application_disconected_32x32.png",
+                "application_connected_32x32.png",
+                "media_playback_start_32x32.png",
                 "media_playback_pause_32x32.png",
-                "media_playback_stop_32x32",
+                "media_playback_stop_32x32.png",
                 "media_record_32x32.png",
                 "media_seek_backward_32x32.png",
                 "media_seek_forward_32x32.png",
@@ -83,9 +107,9 @@ class MyArtProvider(wx.ArtProvider):
         NamesArtId = set()
         if artid in self.defaultIconMappingActionName.keys():
             NamesArtId = self.defaultIconMappingActionName[artid]
-            if "ART_APPLICATION_STATUS_DISCONECTED" == artid:
-                self.log.info("ART_APPLICATION_STATUS_DISCONECTED")   
-                return wxIcons.trayDefault.GetBitmap()
+            #if "ART_APPLICATION_STATUS_DISCONECTED" == artid:
+            #    self.log.info("ART_APPLICATION_STATUS_DISCONECTED")   
+            #    return wxIcons.trayDefault.GetBitmap()
                 
 
         NamesSize = set()
@@ -94,21 +118,27 @@ class MyArtProvider(wx.ArtProvider):
             NamesSize = self.defaultIconMappingSize[IconSize]
         Found = NamesSize.intersection(NamesArtId)
         numberFound = len(Found)
+        if numberFound == 0:
+            self.log.warn("No image found for  %s:%s:%s" %(artid, client, size)) 
+            self.log.warn(NamesArtId)
+            self.log.warn()
         for item in Found:
             self.log.debug("found one")
-            filePath = "icons/%s" % item
+            filePath = "%s/icons/%s" % (self.baseFolder,item)
             if os.path.isfile(filePath):
                 self.log.debug("exists one")
                 fp = open(filePath)
                 bmp = wx.BitmapFromImage(wx.ImageFromStream(fp))
                 fp.close()
+                self.log.info("Setting Path %s" %(filePath)) 
                 break
-   
+            else:
+                self.log.warn("Bad Path %s" %(filePath)) 
         # but be more specific for these   
          
          
    
         if bmp.Ok():   
-            self.log.info("MyArtProvider: providing %s:%s at %s" %(artid, client, size))   
+            self.log.debug("MyArtProvider: providing %s:%s at %s" %(artid, client, size))   
         return bmp   
    
