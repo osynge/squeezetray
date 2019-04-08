e631b7663a5b5f07074a84628401aeb932b761a4
Owen Synge <owen.synge@jaysnest.de>
2019-04-08 08:49:09 +0200

    pycodestyle:E231 missing whitespace after ','

--- sqtray/wxTaskBarIcon.py
+++ sqtray/wxTaskBarIcon.py
@@ -15,20 +15,20 @@ from sqtray.models import Observable
 
 from sqtray.wxTrayIconPopUpMenu import  PopupMenuPresentor
 class TaskBarIcon(wx.TaskBarIcon):
-    def __init__(self,model):
+    def __init__(self, model):
         super(TaskBarIcon, self).__init__()
         self.model = model
         self.icon = wx.ArtProvider.GetIcon(
             "ART_APPLICATION_STATUS_DISCONECTED",
             wx.ART_OTHER,
-            (16,16))
-        self.SetIcon(self.icon,"SqueezeTray")
+            (16, 16))
+        self.SetIcon(self.icon, "SqueezeTray")
         self.Example = None
         self.CallBacks = { 'CreatePopupMenu' : []}
         self.IconStatus = None
         self.IconSize = None
         self.log = logging.getLogger("TaskBarIcon")
-    def CbAddCreatePopupMenu(self,funct):
+    def CbAddCreatePopupMenu(self, funct):
         """Callback items shoudl return Menu objects"""
         if 'CreatePopupMenu' in self.CallBacks:
             self.CallBacks['CreatePopupMenu'].append(funct)
@@ -39,19 +39,19 @@ class TaskBarIcon(wx.TaskBarIcon):
         super(TaskBarIcon, self).Show()
 
     def GetSqueezeServerPlayer(self):
-        if not hasattr(self,'model'):
+        if not hasattr(self, 'model'):
             return None
         return self.model.GuiPlayer.get()
 
 
 
-    def set_icon(self, status,size):
+    def set_icon(self, status, size):
         if (self.IconStatus == status) and (self.IconSize == size):
             self.log.debug("Icon unchanged")
             return
 
-        self.log.debug("Icon changed from '%s:%s'" % (self.IconStatus,str(self.IconSize).strip()))
-        self.log.debug("Icon changed to '%s:%s'" % (status,size))
+        self.log.debug("Icon changed from '%s:%s'" % (self.IconStatus, str(self.IconSize).strip()))
+        self.log.debug("Icon changed to '%s:%s'" % (status, size))
         self.IconStatus = status
         self.IconSize = size
 
@@ -60,22 +60,22 @@ class TaskBarIcon(wx.TaskBarIcon):
             self.log.error("status == None")
             return
         #self.icon = wx.ArtProvider.GetIcon(status, "WIBBLE",size)
-        testIcon = wx.ArtProvider.GetIcon(self.IconStatus,wx.ART_OTHER ,size)
+        testIcon = wx.ArtProvider.GetIcon(self.IconStatus, wx.ART_OTHER, size)
         if not testIcon.Ok():
             self.log.debug("Icon not OK")
             return
         self.icon = testIcon
         CurrentToolTip = TRAY_TOOLTIP
-        if hasattr(self,'ToolTipText'):
+        if hasattr(self, 'ToolTipText'):
             CurrentToolTip = self.ToolTipText
         self.SetIcon(self.icon, CurrentToolTip)
 
     def set_toolTip(self, tooltip):
-        if hasattr(self,'ToolTipText'):
+        if hasattr(self, 'ToolTipText'):
             if self.ToolTipText == tooltip:
-                self.log.debug("Icon changed '%s:%s'" % (self.IconStatus,str(self.IconSize).strip()))
+                self.log.debug("Icon changed '%s:%s'" % (self.IconStatus, str(self.IconSize).strip()))
                 return
-        if hasattr(self,'icon'):
+        if hasattr(self, 'icon'):
             self.SetIcon(self.icon, tooltip)
         self.ToolTipText = unicode(tooltip)
     def CreatePopupMenu(self):
@@ -152,7 +152,7 @@ class TaskBarIconUpdateInteractor(object):
         self.view = view
         self.model.tooltip.addCallback(self._OnToolTipChange)
 
-    def _OnToolTipChange(self,event):
+    def _OnToolTipChange(self, event):
         self.view.set_toolTip(ToolTip)
 
         self.log.warn("dsfsf")
