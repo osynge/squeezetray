f6331f893e6b3912198df0551879a7be9b382bd9
Owen Synge <owen.synge@jaysnest.de>
2019-04-08 05:21:47 +0200

    pycodestyle:W293 blank line contains whitespace

--- sqtray/wxFrmNowPlayingPresentor.py
+++ sqtray/wxFrmNowPlayingPresentor.py
@@ -11,12 +11,12 @@ class mdlFrmInternalNowPlaying(mdlFrmNowPlaying):
     def __init__(self):
         self.__init__(mdlFrmNowPlaying)
         self.wxComboPlayerOptions = ObservableDict()
-           
-     
+
+
 
 class frmPlayingPresentor:
     def __init__(self,model,interactor):
-        
+
         self.GuiModel = model
         self.settingsOpen = False
         self.Example = None
@@ -36,29 +36,29 @@ class frmPlayingPresentor:
         if self.settingsOpen == True:
             return
         self.Example = FrmNowPlaying(None, title='Now Playing')
-        
+
         self.Example.Bind(wx.EVT_CLOSE, self.ViewClose)
         self.updateGuiInteractor.installGui(self.Example)
-        
+
         self.Example.cbAddOnQuit(self.OnQuit)
         self.Example.cbAddOnPause(self.interactor.cbAddOnPause)
-        
-        
+
+
         self.Example.Show()
-        
+
         self.settingsOpen = True
-        
+
     def ViewClose(self,evnt = None):
         self.settingsOpen = False
         if self.Example != None:
             self.updateGuiInteractor.installGui(None)
             self.Example.Destroy()
         self.Example = None
-        
+
     def OnQuit(self,iconName):
         self.ViewClose(None)
         self.interactor.doCbOnExit(None)
-    
+
     def Update(self):
         self.updateGuiInteractor.updateTrackEnds()
         self.updateGuiInteractor.updatePlayerStatus()
