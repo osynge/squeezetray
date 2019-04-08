7f1d5d95290c7839562daa4060dbb60371bd96f1
Owen Synge <owen.synge@jaysnest.de>
2019-04-08 05:28:26 +0200

    pycodestyle:W293 blank line contains whitespace

--- sqtray/modelsWxFrmNowPlaying.py
+++ sqtray/modelsWxFrmNowPlaying.py
@@ -13,15 +13,15 @@ class mdlFrmNowPlaying:
         self.CurrentPlayerIdDefault = Observable(None)
         self.nowPlayPlayerId = Observable(None)
         self.CurrentPlayerStatus = Observable(None)
-        
+
         self.CurrentTrackId = Observable(None)
         self.CurrentTrackArtist = Observable(None)
         self.CurrentTrackName = Observable(None)
         self.CurrentTrackAlbum = Observable(None)
-        
+
         self.CurrentTrackEnds = Observable(None)
         self.CurrentTrackDuration = Observable(None)
         self.showingPlayer = Observable(0)
         self.connectionMsg = Observable(None)
         self.statusText = Observable(None)
-        
+
