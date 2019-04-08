becc9507709b9e6435df504b353d6433dac87b7e
Owen Synge <owen.synge@jaysnest.de>
2019-04-08 09:08:53 +0200

    pycodestyle:E231 missing whitespace after ','

--- sqtray/wxAppInteractor.py
+++ sqtray/wxAppInteractor.py
@@ -11,77 +11,77 @@ class GuiInteractor(object):
             "on_random_songs" : {}
         }
 
-    def cbAddOnExit(self,func):
+    def cbAddOnExit(self, func):
         self.callbacks['on_exit'][func] = 1
 
-    def cbAddOnSettings(self,func):
+    def cbAddOnSettings(self, func):
         self.callbacks['on_settings'][func] = 1
 
-    def cbAddOnNowPlaying(self,func):
+    def cbAddOnNowPlaying(self, func):
         self.callbacks['on_nowPlaying'][func] = 1
 
-    def cbAddOnPlay(self,func):
+    def cbAddOnPlay(self, func):
         self.callbacks['on_play'][func] = 1
 
-    def cbAddOnPause(self,func):
+    def cbAddOnPause(self, func):
         self.callbacks['on_pause'][func] = 1
-    def cbAddOnStop(self,func):
+    def cbAddOnStop(self, func):
         self.callbacks['on_stop'][func] = 1
 
 
 
-    def cbAddOnSeekIndex(self,func):
+    def cbAddOnSeekIndex(self, func):
         self.callbacks['on_seek_index'][func] = 1
 
-    def cbAddOnRandomSongs(self,func):
+    def cbAddOnRandomSongs(self, func):
         self.callbacks['on_random_songs'][func] = 1
 
 
-    def doCbOnExit(self,evt):
+    def doCbOnExit(self, evt):
         results = {}
         for item in self.callbacks["on_exit"]:
             results[item] = item()
         return results
 
-    def doCbOnSettings(self,evt):
+    def doCbOnSettings(self, evt):
         results = {}
         for item in self.callbacks["on_settings"]:
             results[item] = item()
         return results
-    def doCbOnNowPlaying(self,evt):
+    def doCbOnNowPlaying(self, evt):
         results = {}
         for item in self.callbacks["on_nowPlaying"]:
             results[item] = item()
         return results
-    def doCbOnPlay(self,evt,player):
+    def doCbOnPlay(self, evt, player):
         results = {}
         for item in self.callbacks["on_play"]:
             results[item] = item(player)
         return results
-    def doCbOnStop(self,evt,player):
+    def doCbOnStop(self, evt, player):
         results = {}
         for item in self.callbacks["on_stop"]:
             results[item] = item(player)
         return results
-    def doCbOnPause(self,evt,player):
+    def doCbOnPause(self, evt, player):
         results = {}
         for item in self.callbacks["on_pause"]:
             results[item] = item(player)
         return results
 
-    def doCbOnSeekForward(self,evt,player):
+    def doCbOnSeekForward(self, evt, player):
         results = {}
         for item in self.callbacks["on_seek_index"]:
-            results[item] = item(player,1)
+            results[item] = item(player, 1)
         return results
 
-    def doCbOnSeekBackwards(self,evt,player):
+    def doCbOnSeekBackwards(self, evt, player):
         results = {}
         for item in self.callbacks["on_seek_index"]:
-            results[item] = item(player,-1)
+            results[item] = item(player, -1)
         return results
 
-    def doCbOnRandomSongs(self,evt,player):
+    def doCbOnRandomSongs(self, evt, player):
         results = {}
         for item in self.callbacks["on_random_songs"]:
             results[item] = item(player)
