1c535f65fa2af8d92005c667db92f14eac066287
Owen Synge <owen.synge@jaysnest.de>
2019-04-08 10:06:23 +0200

    E301 expected 1 blank line, found 0

--- sqtray/wxFrmSettings.py
+++ sqtray/wxFrmSettings.py
@@ -1,5 +1,3 @@
-
-
 import wx
 from wxEvents import EVT_RESULT_CONNECTED_ID
 from wxEvents import EVT_RESULT_PLAYERS_ID
@@ -31,50 +29,31 @@ class FrmSettings(wx.Frame):
         self.BtnApply = wx.Button(self, -1, "Apply")
         self.BtnCancel = wx.Button(self, -1, "Cancel")
         self.BtnSave = wx.Button(self, -1, "Save")
-
-
         self.Bind(wx.EVT_BUTTON, self.OnCancel, id=self.BtnCancel.GetId())
         self.Bind(wx.EVT_BUTTON, self.OnSave, id=self.BtnSave.GetId())
         self.Bind(wx.EVT_BUTTON, self.OnApply, id=self.BtnApply.GetId())
-
-
-
-
         self.sizer.Add(self.BtnApply, (8, 0), wx.DefaultSpan, wx.EXPAND)
         self.sizer.Add(self.BtnCancel, (8, 1), wx.DefaultSpan, wx.EXPAND)
         self.sizer.Add(self.BtnSave, (8, 2), wx.DefaultSpan, wx.EXPAND)
-
         label1 = wx.StaticText(self, -1, 'Host:')
-
         self.sizer.Add(label1, (0, 0), wx.DefaultSpan, wx.EXPAND)
-
         self.tcHost = wx.TextCtrl(self, -1 )
         self.sizer.Add(self.tcHost , (0, 1), (1, 2), wx.EXPAND)
         label2 = wx.StaticText(self, -1, 'Port:')
-
-
         self.sizer.Add(label2, (1, 0), wx.DefaultSpan, wx.EXPAND)
         label3 = wx.StaticText(self, -1, 'Player:')
-
         self.sizer.Add(label3, (2, 0), wx.DefaultSpan, wx.EXPAND)
-
         self.scPort = wx.SpinCtrl(self, -1, unicode(9000),  min=1, max=99999)
         self.sizer.Add(self.scPort, (1, 1), wx.DefaultSpan, wx.EXPAND)
         #self.statusbar = self.CreateStatusBar()
         #self.sizer.Add(self.statusbar, (9, 0),(2,9), wx.EXPAND)
-
         self.cbPlayer = wx.ComboBox(self, -1, style=wx.CB_READONLY)
         self.sizer.Add(self.cbPlayer, (2, 1), (1, 2), wx.EXPAND)
-
-
         self.sizer.AddGrowableRow(8)
         self.sizer.AddGrowableCol(0)
         self.sizer.AddGrowableCol(1)
         self.sizer.AddGrowableCol(2)
-
         self.SetSizerAndFit(self.sizer)
-
-
         self.icon = wxIcons.trayDefault.getIcon()
         self.SetIcon(self.icon)
         self.IconStatus = None
@@ -84,7 +63,6 @@ class FrmSettings(wx.Frame):
         self.GuiModel.statusText.addCallback(self.onStatusText)
         self.GuiModel.host.addCallback(self.onHost)
         self.GuiModel.port.addCallback(self.onPort)
-
         self.CurrentStatusText = None
 
     def ModelSet(self, model):
@@ -124,6 +102,7 @@ class FrmSettings(wx.Frame):
 
     def onHost(self, value):
         self.tcHost.SetValue(self.GuiModel.host.get())
+
     def onPort(self, value):
         self.scPort.SetValue(self.GuiModel.port.get())
 
@@ -140,6 +119,7 @@ class FrmSettings(wx.Frame):
         oldPort = self.model.port.get()
         self.model.port.update(newPort)
         self.cbDoOnApply()
+
     def OnCancel(self, event):
         #self.FrmCtrl.closeSettings(event)
         #self.app.tb.on_settings_close(event)
